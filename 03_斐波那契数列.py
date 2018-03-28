f2481c032e711ca5cdeb5f5fb4019a9b05aa02e8
2990599771@qq.com <2990599771@qq.com>
2018-03-28 19:49:50 +0800

    添加第三个小程序

---
+++ 03_斐波那契数列.py
@@ -0,0 +1,25 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+'''
+    菲波那切数列递推公式：
+        f(n) = f(n-1) + f(n-2)
+'''
+
+
+def tony(num):
+    if num == 1:
+        return 0
+    if num == 2:
+        return 1
+    if num > 2:
+        return tony(num - 1) + tony(num - 2)
+
+
+def main():
+    in_num = int(input("请输入你要计算的值:"))
+    for number in range(1, in_num + 1):
+        print(tony(number), "\t", end="")
+
+
+if __name__ == '__main__':
+    main()
