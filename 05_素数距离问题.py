93b0b2add78eab0eb5de0713a5a0ceda0ce1d2f9
2990599771@qq.com <2990599771@qq.com>
2018-03-29 12:53:44 +0800

    添加第五个小程序

---
+++ 05_素数距离问题.py
@@ -0,0 +1,55 @@
+# _*_coding:utf-8_*_
+# Author:liu
+'''
+    输入一个整数n，输出整数n相邻最近的两个素数
+    如果左右素数距离相同，则输出左侧素数以及相应的距离
+    如果整数n本身是素数，则输出自己本身，距离为0
+'''
+
+min_long = 0
+min_num = 0
+
+
+def main():
+    global min_long
+    global min_num
+    in_num = int(input("请输入一个整数:"))
+
+    fron_size = 0
+    for front_num in range(in_num, 1, -1):
+        front_result = is_sushu(front_num)
+        if not front_result:
+            min_long = fron_size
+            min_num = front_num
+            print('左边距离:', fron_size)
+            break
+        else:
+            fron_size += 1
+
+    size = 0
+    while True:
+        result = is_sushu(in_num)
+        # 是素数
+        if not result:
+            print('右边距离:', size)
+            if size < min_long:
+                print('最短距离：', size, '素数：', in_num)
+
+            else:
+                print('最短距离:', min_long, '素数:', min_num)
+            break
+        else:
+            size += 1
+            in_num += 1
+
+
+# 判断是不是素数
+def is_sushu(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return 1
+    return 0
+
+
+if __name__ == '__main__':
+    main()
